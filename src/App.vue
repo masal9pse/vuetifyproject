dee095b6beb66e4fa35ef2172afa99d68dfad6ed
masal9pse <returnymgstokh@icloud.com>
2019-10-07 12:03:56 +0900

    50

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,33 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-side-icon @click="openSideMenu"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.stop="toggleSideMenu"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>マイアドレス帳</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
     <SideNav />
+
     <v-content></v-content>
   </v-app>
 </template>
 
 <script>
 import SideNav from "./components/SideNav";
+import { mapActions } from "vuex";
 export default {
   name: "App",
   components: {
     SideNav
   },
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      //
+    };
+  },
   methods: {
-    openSideMenu() {
-      this.$store.dispatch("toggleSideMenu");
-    }
+    ...mapActions(["toggleSideMenu"])
   }
 };
 </script>
