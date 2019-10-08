1f46a1bb4e78fd22483ac9bbacd6330876f691f4
masal9pse <returnymgstokh@icloud.com>
2019-10-08 12:32:00 +0900

    57

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//Initialize Firebase
+const config = {
+  apiKey: "AIzaSyC2iXgItUN3maIhRvq-fx7cacuSC6A69EE",
+  authDomain: "my-address-pj2-56e59.firebaseapp.com",
+  databaseURL: "https://my-address-pj2-56e59.firebaseio.com",
+  projectId: "my-address-pj2-56e59",
+  storageBucket: "",
+  messagingSenderId: "828590639649",
+  appId: "1:828590639649:web:c0ddb65e3d2269a8d33160",
+  measurementId: "G-P8GJ5JZ04N"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
